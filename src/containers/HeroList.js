c785a6110112c1123fd7dae1b083e1756ed6ac7a
Julian <artjulius@hotmail.com>
2020-08-10 10:53:41 -0500

    Add HeroList container to display all heroes found in the Redux store after user sends an input value

---
+++ src/containers/HeroList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import HeroCard from "./HeroCard";
+import { connect } from "react-redux";
+import { updateFromReduxStore } from "../actions/index";
+// era funcion, implementar
+class HeroList extends React.Component {
+  componentDidMount() {
+    const { updateFromReduxStore } = this.props;
+    updateFromReduxStore();
+  }
+
+  render() {
+    const { heroResults } = this.props;
+    console.log(heroResults);
+    return (
+      <div className="POPOSList">
+        {heroResults.map((hero, index) => (
+          <HeroCard
+            id={index}
+            key={index}
+            name={hero.name}
+            image={hero.image.url}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  heroResults: state.heroReducer.heroResults,
+});
+
+const matchDispatchToProps = (dispatch) => ({
+  updateFromReduxStore: () => {
+    dispatch(updateFromReduxStore());
+  },
+});
+
+export default connect(mapStateToProps, matchDispatchToProps)(HeroList);
