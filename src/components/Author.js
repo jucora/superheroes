10bfece6438fc9da1d247e08895ad2a076890d75
Julian <artjulius@hotmail.com>
2020-08-10 10:39:40 -0500

    Add Author component

---
+++ src/components/Author.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Author() {
+  return (
+    <div>
+      <h1>JULIAN BELMONTE</h1>
+      <p>I'm a Software Developer!</p>
+    </div>
+  );
+}
+
+export default Author;
