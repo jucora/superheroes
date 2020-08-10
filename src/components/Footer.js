f9c0bbdd1ff4e7f2c2820c863465b61142b7e6e4
Julian <artjulius@hotmail.com>
2020-08-10 10:41:13 -0500

    Add basic footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footerContainer">
+      <footer>
+        <p>Julian Belmonte copyright 2020</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
