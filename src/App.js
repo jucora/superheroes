b477b9b7a2dbffd8a2196a0b727b9952eeb104fe
Julian <artjulius@hotmail.com>
2020-08-10 10:38:14 -0500

    Add App.js component and its main structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
-import "./App.css";
+import Title from "./Title";
+import HeroList from "./HeroList";
+import Footer from "./Footer";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Author from "./Author";
+import HeroDetails from "./HeroDetails";
 
-function App() {
-  return <div className="App"></div>;
-}
+import HeroForm from "./HeroForm";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../reducers/index";
+const store = createStore(rootReducer);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Title />
+          {window.location.pathname === "/" ? <HeroForm /> : null}
+          <Route exact path="/" component={HeroList} />
+          <Route path="/about" component={Author} />
+          <Route path="/details/:id" component={HeroDetails} />
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  );
+};
+
+export { store };
 export default App;
