20222cd6cb1c069d1edd840bebef9ae976a83323
Julian <artjulius@hotmail.com>
2020-08-10 11:03:04 -0500

    Add index file inside actions folder

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const UPDATE_HEROES = "UPDATE_HEROES";
+export const UPDATE = "UPDATE";
+
+export const updateHeroes = (results) => ({
+  type: UPDATE_HEROES,
+  payload: results,
+});
+
+export const updateFromReduxStore = () => ({
+  type: UPDATE,
+});
