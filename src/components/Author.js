17c349a1797bfe670f028a446cdf62ac2c5ce2b1
Julian <artjulius@hotmail.com>
2020-08-11 11:37:46 -0500

    Add info about author

--- src/components/Author.js
+++ src/components/Author.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const Author = () => {
   return (
-    <div>
+    <div className="author">
       <h1>JULIAN BELMONTE</h1>
-      <p>I&apos;m a Software Developer!</p>
+      <img src={require("../img/julian.jpg")}></img>
+      <div className={"portfolioLink"}>
+        <a href="https://jucora.github.io/" target="_blank">
+          <h2>Please visit my portfolio</h2>
+        </a>
+      </div>
     </div>
   );
 };
